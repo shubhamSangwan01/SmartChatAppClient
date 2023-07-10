9f1b548339cc8f97c898379657a9b6b37557e942
Ashish Kumar <79131438+shubhamSangwan01@users.noreply.github.com>
2023-07-11 00:37:03 +0530

    First commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,154 @@
+import React from "react";
+import "../styles/loginForm.css";
+
+const Form = ({
+  formType,
+  loginFormData,
+  handleLoginFormChange,
+  signupFormData,
+  handleSignupFormChange,
+  handleLoginSubmit,
+}) => {
+  console.log(loginFormData);
+  return (
+    <div className="loginForm__outer">
+      {formType == "login" && (
+        <form className="loginForm" onSubmit={handleLoginSubmit}>
+          <ul className="loginForm__list">
+            <li className="loginForm__input">
+              <input
+                type="email"
+                name="email"
+                value={loginFormData.email}
+                onChange={handleLoginFormChange}
+                placeholder="Enter Email"
+              />
+              <span className="Icon material-symbols-outlined">cancel</span>
+            </li>
+            <li className="loginForm__input">
+              <input
+                type="password"
+                name="password"
+                value={loginFormData.password}
+                onChange={handleLoginFormChange}
+                placeholder="Enter Password"
+              />
+              <span
+                class="Icon material-symbols-outlined"
+                style={{ color: "#3B0DF4" }}
+              >
+                visibility_off
+              </span>
+            </li>
+            <li id="loginForm__resetPassword">
+              <span>Reset Password ?</span>
+            </li>
+            <li id="loginForm__signIn">
+              <button type="submit">Sign In</button>
+            </li>
+            <li id="loginForm__line__li">
+              <div className="loginForm__line"></div>
+              <span>Or continue with</span>
+              <div className="loginForm__line"></div>
+            </li>
+            <li id="loginForm__buttons">
+              <ul className="loginForm__buttons__list">
+                <li id="google">
+                  <img src="/images/google.png" alt="Google" />
+                </li>
+                <li id="apple">
+                  <img src="/images/apple.png" alt="Google" />
+                </li>
+                <li id="facebook">
+                  <img src="/images/facebook.png" alt="Google" />
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </form>
+      )}
+      {formType == "register" && (
+        <form className="loginForm" onSubmit={handleLoginSubmit}>
+          <ul className="loginForm__list">
+            <li className="loginForm__input">
+              <input
+                type="text"
+                name="name"
+                value={signupFormData.name}
+                onChange={handleSignupFormChange}
+                placeholder="Enter Name"
+              />
+              <span className="Icon material-symbols-outlined">cancel</span>
+            </li>
+            <li className="loginForm__input">
+              <input
+                type="email"
+                name="email"
+                value={signupFormData.email}
+                onChange={handleSignupFormChange}
+                placeholder="Enter Email"
+              />
+              <span className="Icon material-symbols-outlined">cancel</span>
+            </li>
+            <li className="loginForm__input">
+              <input
+                type="password"
+                name="password"
+                value={signupFormData.password}
+                onChange={handleSignupFormChange}
+                placeholder="Enter Password"
+              />
+              <span
+                class="Icon material-symbols-outlined"
+                style={{ color: "#3B0DF4" }}
+              >
+                visibility_off
+              </span>
+            </li>
+            <li className="loginForm__input">
+              <input
+                type="password"
+                name="confirmPassword"
+                value={signupFormData.confirmPassword}
+                onChange={handleSignupFormChange}
+                placeholder="Confirm Password"
+              />
+              <span
+                class="Icon material-symbols-outlined"
+                style={{ color: "#3B0DF4" }}
+              >
+                visibility_off
+              </span>
+            </li>
+            <li id="loginForm__resetPassword">
+              <span>Reset Password ?</span>
+            </li>
+            <li id="loginForm__signIn">
+              <button type="submit">Sign In</button>
+            </li>
+            <li id="loginForm__line__li">
+              <div className="loginForm__line"></div>
+              <span>Or continue with</span>
+              <div className="loginForm__line"></div>
+            </li>
+            <li id="loginForm__buttons">
+              <ul className="loginForm__buttons__list">
+                <li id="google">
+                  <img src="/images/google.png" alt="Google" />
+                </li>
+                <li id="apple">
+                  <img src="/images/apple.png" alt="Google" />
+                </li>
+                <li id="facebook">
+                  <img src="/images/facebook.png" alt="Google" />
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "../styles/login.css";
+import LoginNavbar from "../components/LoginNavbar";
+import Form from "../components/Form";
+
+
+const Login = () => {
+  const [formType, setFormType] = React.useState("login");
+  const [loginFormData, setLoginFormData] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const handleFormTypeChange = (type) => {
+    setFormType(type);
+  };
+
+  const [signupFormData, setSignupFormData] = React.useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleLoginFormChange = (e) => {
+    const { name, value } = e.target;
+    setLoginFormData((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSignupFormChange = (e) => {
+    const { name, value } = e.target;
+    setSignupFormData((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login__outer">
+      <LoginNavbar handleFormTypeChange={handleFormTypeChange} />
+      <section>
+        <div className="loginSection_text">
+          <div className="loginSection_textBackground1"></div>
+          <div className="loginSection_textBackground2"></div>
+          <h1>Sign In</h1>
+          <h1>to enjoy!</h1>
+          <p>If you don't an account </p>
+          <p>you can Register here!</p>
+        </div>
+        <div className="loginSection_image">
+          <img src="/images/loginMan.png" alt="" />
+        </div>
+        <div>
+          
+          <Form
+            formType={formType}
+            setFormType={setFormType}
+            loginFormData={loginFormData}
+            setLoginFormData={setLoginFormData}
+            signupFormData={signupFormData}
+            setSignupFormData={setSignupFormData}
+            handleFormTypeChange={handleFormTypeChange}
+            handleLoginFormChange={handleLoginFormChange}
+            handleSignupFormChange={handleSignupFormChange}
+            handleLoginSubmit={handleLoginSubmit}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
