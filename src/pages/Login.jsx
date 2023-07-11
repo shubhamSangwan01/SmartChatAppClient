d4139c2e847b399740a50fda47e8dd70b9b58966
Shubham Sangwan <79131438+shubhamSangwan01@users.noreply.github.com>
2023-07-11 13:31:28 +0530

    login page text updates

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,10 +41,8 @@ const Login = () => {
         <div className="loginSection_text">
           <div className="loginSection_textBackground1"></div>
           <div className="loginSection_textBackground2"></div>
-          <h1>Sign In</h1>
-          <h1>to enjoy!</h1>
-          <p>If you don't an account </p>
-          <p>you can Register here!</p>
+          <h1>Sign In <br />to Enjoy!</h1>
+          <p>If you don't have an account <br />you can Register here!</p>
         </div>
         <div className="loginSection_image">
           <img src="/images/loginMan.png" alt="" />
